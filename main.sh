b7fd0c872d39494a2507a45ec6bf75404fc12c3b
Leonardo Ventura <leonardo.ventura@ufrj.br>
2018-05-15 01:24:17 -0300

    ops

--- main.sh
+++ main.sh
@@ -2,6 +2,6 @@
 
 lines=$(echo $(($(echo -n "quit" | gdb -q --eval="disas main" $1 | wc -l) - 3)))
 
-shellcode=$(objdump -d shell | grep -A${lines} "<main>" | grep -v "<main>" | cut -f 2 | xargs | sed -e "s/\s/\\\x/g")
+shellcode=$(objdump -d $1 | grep -A${lines} "<main>" | grep -v "<main>" | cut -f 2 | xargs | sed -e "s/\s/\\\x/g")
 
 echo "\\x${shellcode}"
